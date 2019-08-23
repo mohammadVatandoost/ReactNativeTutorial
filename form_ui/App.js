b936d41a439358cfd879cb4160f7076fb88fe282
Mohammad Vatandoost <mohamadvatandoost512@gmail.com>
2019-08-23 13:15:48 -0700

    create form_ui project

---
+++ form_ui/App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Fragment} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import RegForm1 from './Components/RegForm1/RegForm1';
+
+const App = () => {
+  return (
+    <RegForm1/>
+  );
+};
+
+
+
+export default App;
---
+++ form_ui/Components/RegForm1/RegForm1.js
@@ -0,0 +1,51 @@
+import React, {Fragment, Component } from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    TouchableOpacity,
+    View,
+    Text,
+    TextInput,
+} from 'react-native';
+
+
+class RegForm1 extends Component {
+
+    render() {
+
+        return (
+            <View style={styles.container}>
+                <View style={styles.regForm}>
+                    <Text style={styles.header}>Registeration</Text>
+                    <TextInput style={styles.textInput} placeholder="Your name" placeholderTextColor="#FFF" underlineColorAndroid={'transparent'} />
+                    <TextInput style={styles.textInput} placeholder="Your email" placeholderTextColor="#FFF" underlineColorAndroid={'transparent'} />
+                    <TextInput style={styles.textInput} placeholder="Your password" secureTextEntry={true} placeholderTextColor="#FFF" underlineColorAndroid={'transparent'} />
+                    <TouchableOpacity style={styles.button}><Text style={styles.btnText}>Sign up</Text></TouchableOpacity>
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, justifyContent: 'center', paddingRight: 60, paddingLeft: 60, backgroundColor: '#36485F'
+    },
+    regForm: {
+        alignSelf: 'stretch'
+    },
+    header: {
+        fontSize: 24, color: '#FFF', paddingBottom: 10, marginBottom: 40, borderBottomWidth: 1,
+        borderBottomColor: '#199187'
+    },
+    textInput: {
+        alignSelf: 'stretch', height: 40, marginBottom: 30, color: '#FFF', borderBottomWidth: 1,
+        borderBottomColor: '#f8f8f8'
+    },
+    button: {
+        alignSelf: 'stretch', alignItems: 'center', padding: 20, marginBottom: 20, backgroundColor: '#59cbbd'
+    },
+    btnText: {color: '#FFF', fontWeight: 'bold'}
+});
+
+export default RegForm1;
